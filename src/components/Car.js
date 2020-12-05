94706cafa51e1fc3c04ce4283351778a79a70d9d
Shelby-Jean <shelby.gottschall@gmail.com>
2020-12-05 16:54:18 -0500

    updated fileredCar

--- src/components/Car.js
+++ src/components/Car.js
@@ -4,7 +4,7 @@ import { Container, Paper, Chip } from '@material-ui/core';
 
 const Car = (props) => {
     const id = props.match.params.id;
-    const filteredCar = cars.filter(car => car.id == id);
+    const filteredCar = cars.find(car => car.id == id);
     console.log(filteredCar);
 
     return (
@@ -12,19 +12,19 @@ const Car = (props) => {
             <Container maxWidth="sm" className="car-container">
                 <Paper className="car-paper">
                     <div>
-                    <h3>{filteredCar[0].Name}</h3>
+                    <h3>{filteredCar.Name}</h3>
                     </div>
                     <div>
-                        <Chip size="medium" label={`id: ${filteredCar[0].id}`} />
-                        <Chip size="medium" label={`Name: ${filteredCar[0].Name}`} />
-                        <Chip size="medium" label={`Miles_per_Gallon: ${filteredCar[0].Miles_per_Gallon}`} />
-                        <Chip size="medium" label={`Cylinders: ${filteredCar[0].Cylinders}`} />
-                        <Chip size="medium" label={`Displacement: ${filteredCar[0].Displacement}`} />
-                        <Chip size="medium" label={`Horsepower: ${filteredCar[0].Horsepower}`} />
-                        <Chip size="medium" label={`Weight_in_lbs: ${filteredCar[0].Weight_in_lbs}`} />
-                        <Chip size="medium" label={`Acceleration: ${filteredCar[0].Acceleration}`} />
-                        <Chip size="medium" label={`Year: ${filteredCar[0].Year}`} />
-                        <Chip size="medium" label={`Origin: ${filteredCar[0].Origin}`} />
+                        <Chip size="medium" label={`id: ${filteredCar.id}`} />
+                        <Chip size="medium" label={`Name: ${filteredCar.Name}`} />
+                        <Chip size="medium" label={`Miles_per_Gallon: ${filteredCar.Miles_per_Gallon}`} />
+                        <Chip size="medium" label={`Cylinders: ${filteredCar.Cylinders}`} />
+                        <Chip size="medium" label={`Displacement: ${filteredCar.Displacement}`} />
+                        <Chip size="medium" label={`Horsepower: ${filteredCar.Horsepower}`} />
+                        <Chip size="medium" label={`Weight_in_lbs: ${filteredCar.Weight_in_lbs}`} />
+                        <Chip size="medium" label={`Acceleration: ${filteredCar.Acceleration}`} />
+                        <Chip size="medium" label={`Year: ${filteredCar.Year}`} />
+                        <Chip size="medium" label={`Origin: ${filteredCar.Origin}`} />
                     </div>
                 </Paper>
             </Container>
